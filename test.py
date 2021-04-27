be013cce3f1bd2d861151066840aa32333c22af9
alexandertu <alexander.tuoma@gmail.com>
2021-04-27 10:54:24 +0200

    Add template for test and train

--- test.py
+++ test.py
@@ -1,1 +1,39 @@
-#Todo used for testing locally on CPU
+from absl import app
+from absl import flags
+from pathlib import Path
+import pandas as pd
+import os
+from scipy.io import wavfile
+import scipy.io
+import subprocess
+import torchaudio
+import matplotlib.pyplot as plt
+import numpy as np
+
+from datasets.data_loaders import TimitDataset
+from datasets.data_transformations import MFCC
+
+FLAGS = flags.FLAGS
+
+
+def main(argv):
+    del argv  # Unused.
+
+    dataset = TimitDataset(csv_file='test_data.csv',
+                           root_dir='../timit',
+                           transform=MFCC(n_fft=FLAGS.n_fft,
+                                          preemphasis_coefficient=FLAGS.preemphasis_coefficient,
+                                          num_ceps=FLAGS.num_ceps))
+
+    #Todo used for testing locally on CPU
+
+
+
+if __name__ == '__main__':
+    flags.DEFINE_integer('n_fft', 512, 'Size of FFT')
+    flags.DEFINE_float('preemphasis_coefficient', 0.97,
+                       'Coefficient for use in signal preemphasis')
+    flags.DEFINE_integer(
+        'num_ceps', 13, ' Number of cepstra in MFCC computation')
+
+    app.run(main)
