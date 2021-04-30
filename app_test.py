acddc296b206fae981d298603eb0580795208e27
Georgios Moschovis <geomos@kth.se>
2021-04-30 20:02:21 +0200

    Converted MFCCs to torch tensors

--- app_test.py
+++ app_test.py
@@ -9,6 +9,7 @@ import subprocess
 import torchaudio
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 from datasets.data_loaders import TimitDataset
 from datasets.data_transformations import MFCC
@@ -20,24 +21,43 @@ def main(argv):
     del argv  # Unused.
 
     dataset = TimitDataset(csv_file='test_data.csv',
-                           root_dir='../timit',
+                           # root_dir='../timit',
+                           root_dir = "/home/georgmosh/Documents/SpeechLabs/dt2119_semisup_project/SemiSupervisedLearningASR-main/timit",
                            transform=MFCC(n_fft=FLAGS.n_fft,
                                           preemphasis_coefficient=FLAGS.preemphasis_coefficient,
                                           num_ceps=FLAGS.num_ceps))
 
+    # test1(dataset)
+    test2(dataset)
+
+def test2(dataset):
+    tensors = []
+    print("number of samples available", len(dataset))      # outputs 1680
+
     for i in range(len(dataset)):
-        sample = dataset[i]
+            sample = dataset[i]
+            audio = np.asarray(sample['audio'])
+            tensors.append(torch.tensor(audio.tolist(), dtype=torch.long))
+            if(i == 0):
+                print("dimensions initially:", audio.shape)      # outputs (209, 13)
+    print("number of tensors stored", len(tensors))         # outputs 1680
+    
+    print("datatype:", type(tensors[0]))                          # outputs <class 'torch.Tensor'>
+    print("dimensions:", tensors[0].shape)                     # outputs torch.Size([209, 13])
+    
+def test1(dataset):
+    for i in range(len(dataset)):
+            sample = dataset[i]
 
-        audio = np.asarray(sample['audio'])
+            audio = np.asarray(sample['audio'])
 
-        print("Audio shape: ", audio.shape)
+            print("Audio shape: ", audio.shape)
 
-        plt.pcolormesh(audio)
-        plt.show()
+            plt.pcolormesh(audio)
+            plt.show()
 
-        if i == 1:
-            break
-
+            if i == 1:
+                break
 
 if __name__ == '__main__':
     flags.DEFINE_integer('n_fft', 512, 'Size of FFT')
