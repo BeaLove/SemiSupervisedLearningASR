334ea8891047de7399256451020afb6a2045cd60
BeaLove <beatriceelovely@gmail.com>
2021-05-05 16:05:41 +0200

    loads full data set

--- datasets/TIMITdataset.py
+++ datasets/TIMITdataset.py
@@ -27,7 +27,7 @@ class TimitDataset(Dataset):
                 on a sample.
         """
         self.audio_frame = pd.read_csv(os.path.join(root_dir, csv_file))
-        self.audio_frame = self.audio_frame[self.audio_frame['is_phonetic_file'] == True]
+        self.audio_frame = self.audio_frame[self.audio_frame['is_converted_audio'] == True]
 
         self.root_dir = root_dir
         #self.transform = transform
@@ -59,8 +59,9 @@ class TimitDataset(Dataset):
                                  'p': 33, 't': 34, 'k': 35, 'z': 36, 'zh': 37, 'v': 38, 'f': 39,
                                  'th': 40, 's': 41, 'sh': 42, 'hh': 43, 'hv': 43,
                                  'pcl': 44, 'tcl': 44, 'kcl': 44, 'qcl': 44, 'bcl': 45,
-                                 'dcl': 45, 'gcl': 45, 'epi': 46, 'sil': 47, 'h#': 47, '#h': 47, 'pau': 47}
+                                 'dcl': 45, 'gcl': 45, 'epi': 46, 'sil': 47, 'h#': 47, '#h': 47, 'pau': 47, 'q': 48, 'ax-h': 49}
         self.num_labels = max(self.mergedPhones2int.values()) + 1
+        self.max_length = 777
 
     def __len__(self):
         return len(self.audio_frame)
@@ -76,13 +77,18 @@ class TimitDataset(Dataset):
 
         path = os.path.join(self.root_dir, 'data',
                             self.audio_frame.iloc[idx]['path_from_data_dir'])
+        #print(path)
+        path = os.path.splitext(path)[0]
         path = os.path.splitext(path)[0]
 
+
+
         audio_name = path + '.WAV.wav'
+        #print("audio", audio_name)
 
         audio, self.sample_rate = torchaudio.load(audio_name)
         phonemes_name = path + '.PHN'
-
+        #print('phonemes: ', phonemes_name)
         with open(phonemes_name, 'r') as phonemes_file:
             start = []
             stop = []
@@ -113,16 +119,20 @@ class TimitDataset(Dataset):
         '''
 
         sample = self.get_MFCC(audio)
+        pad = self.max_length - sample.shape[0]
+        #torch.nn.functional.pad(sample, (0, 0, 0, 10))
+
 
         frame_labels = frame_labels[:sample.shape[0]]
 
         encoded_phones = []
-        for phone in frame_labels:
-            encoded_phones.append(self.getPhoneCode(phone))
+        #for phone in frame_labels:
+         #   encoded_phones.append(self.getPhoneCode(phone))
         targets = torch.tensor([self.oneHot(self.num_labels, self.getPhoneCode(phone)) for phone in frame_labels] , dtype=torch.float64)
-        print(targets.shape)
-        sample = {'audio': sample, 'targets': targets}
-        return sample
+        #targets = torch.nn.utils.rnn.pad_packed_sequence(targets, total_length=777)
+        #print(targets.shape)
+        #sample = {'audio': sample, 'targets': targets}
+        return sample, targets
 
 
 
@@ -157,6 +167,30 @@ class TimitDataset(Dataset):
 csv_filename = 'train_data.csv'
 root_dir = '../timit'
 
-dataset = TimitDataset(csv_file=csv_filename, root_dir=root_dir, pre_epmh=0.97, num_ceps=13, n_fft=512, frame_size=20, frame_shift=10)
+train_set = TimitDataset(csv_file=csv_filename, root_dir=root_dir, pre_epmh=0.97, num_ceps=13, n_fft=512, frame_size=20, frame_shift=10)
+print('dataset size', len(train_set))
+max_len = 0
+for i in range(len(train_set)):
+    sample, target = train_set.__getitem__(i)
+    print(sample.shape, target.shape)
+    if sample.shape[0] > max_len:
+        max_len = sample.shape[0]
+        print("new max: ", max_len)
+print("max", max_len)
+print("dataset size", len(train_set))
+test_file = 'test_data.csv'
+
+test_set = TimitDataset(csv_file=test_file, root_dir=root_dir, pre_epmh=0.97, num_ceps=13, n_fft=512, frame_size=20, frame_shift=10)
+
+max_len = 0
+for i in range(len(test_set)):
+    sample, target = test_set.__getitem__(i)
+    print(sample.shape, target.shape)
+    if sample.shape[0] > max_len:
+        max_len = sample.shape[0]
+        print("new max: ", max_len)
+print("max", max_len)
+print("dataset size", len(test_set))
+
 '''sample = dataset.__getitem__(0)
 print(sample)'''
