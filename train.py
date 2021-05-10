c54a94ebc282ab0365ba25c64f7bcb44631f2bd4
alexandertu <alexander.tuoma@gmail.com>
2021-05-10 18:56:12 +0200

    Add flags for epoch, data dir and results

--- train.py
+++ train.py
@@ -6,8 +6,7 @@ from absl import app
 import numpy as np
 from datetime import datetime
 from test import test_model
-
-
+from pathlib import Path
 
 from tqdm import tqdm
 
@@ -19,19 +18,11 @@ from datasets.TIMITdataset import TimitDataset
 FLAGS = flags.FLAGS
 
 
+def makedirs(path):
+    Path(path).mkdir(parents=False, exist_ok=True)
+
+
 def main(args):
-    root_dir = 'timit'
-
-    dataset = TimitDataset(csv_file='train_data.csv', root_dir=root_dir,
-                           pre_epmh=FLAGS.preemphasis_coefficient,
-                           num_ceps=FLAGS.num_ceps, n_fft=FLAGS.n_fft, frame_size=FLAGS.frame_len,
-                           frame_shift=FLAGS.frame_shift)
-    '''important before training a model set model name so checkpoints 
-    and fully trained model gets saved with correct name'''
-    model_name = 'vanillaLSTMfullylabeled.pth'
-    save_folder = os.path.abspath('trained_models')
-    save_path = os.path.join(save_folder, model_name)
-
     if torch.cuda.is_available():
         print('using cuda')
         device = torch.device('cuda:0')
@@ -39,22 +30,39 @@ def main(args):
         print('using cpu')
         device = torch.device('cpu')
 
-    model, avg_val_losses, avg_train_losses = train(dataset, num_epochs= 200)
-    os.makedirs(save_folder, exist_ok=True)
-    torch.save(model.state_dict(), save_path)
+    makedirs(FLAGS.results_save_dir)
+    makedirs(os.path.join(FLAGS.results_save_dir, 'checkpoints'))
 
-    timestamp = str(datetime.now())
-    with open('avg_val_losses.txt', 'a') as valLossFile:
-        valLossFile.write(timestamp)
-        valLossFile.writelines(str(avg_val_losses))
-    with open('avg_train_losses.txt', 'a') as trainLossFile:
-        trainLossFile.write(timestamp)
-        trainLossFile.writelines(str(avg_train_losses))
+    model_name = 'vanillaLSTMfullylabeled.pt'
 
-    test_data = TimitDataset(csv_file='test_data.csv', root_dir=root_dir,
+    save_folder = os.path.abspath(FLAGS.results_save_dir)
+    save_path = os.path.join(save_folder, model_name)
+
+    dataset = TimitDataset(csv_file='train_data.csv', root_dir=FLAGS.dataset_root_dir,
                            pre_epmh=FLAGS.preemphasis_coefficient,
                            num_ceps=FLAGS.num_ceps, n_fft=FLAGS.n_fft, frame_size=FLAGS.frame_len,
                            frame_shift=FLAGS.frame_shift)
+    '''important before training a model set model name so checkpoints 
+    and fully trained model gets saved with correct name'''
+
+    model, avg_val_losses, avg_train_losses = train(
+        dataset, num_epochs=FLAGS.num_epochs)
+
+    torch.save(model.state_dict(), save_path)
+
+    timestamp = str(datetime.now())
+    with open(os.path.abspath(os.path.join(FLAGS.results_save_dir, 'avg_val_losses.txt')), 'a') as valLossFile:
+        valLossFile.write(timestamp)
+        valLossFile.writelines(str(avg_val_losses))
+
+    with open(os.path.abspath(os.path.join(FLAGS.results_save_dir, 'avg_train_losses.txt')), 'a') as trainLossFile:
+        trainLossFile.write(timestamp)
+        trainLossFile.writelines(str(avg_train_losses))
+
+    test_data = TimitDataset(csv_file='test_data.csv', root_dir=FLAGS.dataset_root_dir,
+                             pre_epmh=FLAGS.preemphasis_coefficient,
+                             num_ceps=FLAGS.num_ceps, n_fft=FLAGS.n_fft, frame_size=FLAGS.frame_len,
+                             frame_shift=FLAGS.frame_shift)
 
     accuracy = test.test_model(model, test_data)
 
@@ -68,7 +76,6 @@ def train(dataset, num_epochs, batch_size=1):
     avg_val_losses = []
     patience = 20
 
-    os.makedirs('checkpoints', exist_ok=True)
     val_split = int(len(dataset)*0.15)
     train_data, val_data = torch.utils.data.random_split(dataset, [len(dataset) - val_split, val_split],
                                                          generator=torch.Generator().manual_seed(15))
@@ -79,10 +86,12 @@ def train(dataset, num_epochs, batch_size=1):
         print('using cpu')
         device = torch.device('cpu')
 
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=3, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(
+        train_data, batch_size=batch_size, num_workers=3, shuffle=True)
     '''if batch_size > 1:
         train_loader = torch.nn.utils.rnn.pad_packed_sequence(train_loader)'''
-    val_loader = torch.utils.data.DataLoader(val_data, batch_size=1, num_workers=2)
+    val_loader = torch.utils.data.DataLoader(
+        val_data, batch_size=1, num_workers=2)
     #val_loader = torch.nn.utils.rnn.pad_packed_sequence(val_loader)
 
     loss = nn.CrossEntropyLoss()
@@ -90,7 +99,10 @@ def train(dataset, num_epochs, batch_size=1):
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters())
     #early_stop = EarlyStopping(patience=patience, verbose=True)
-    for epoch in tqdm(range(num_epochs), desc='training epochs'):
+
+    bar = tqdm(range(num_epochs))
+
+    for epoch in bar:
 
         for batch in train_loader:
             sample, target = batch
@@ -102,22 +114,26 @@ def train(dataset, num_epochs, batch_size=1):
             train_losses.append(loss_val.item())
             loss_val.backward()
             optimizer.step()
+
         avg_train_losses.append(np.average(train_losses))
-        print(avg_train_losses[-1])
 
-        if epoch > 9:
-            model.eval()
-            for data, target in tqdm(val_loader):
-                data = data.to(device)
-                target = target.to(device)
-                output = model.forward(data)
-                val_loss = loss(output.squeeze(), target.squeeze())
-                val_losses.append(val_loss.item())
-            avg_val_loss = np.average(val_losses)
-            avg_val_losses.append(avg_val_loss)
-            model.train()
+        model.eval()
+        for data, target in val_loader:
+            data = data.to(device)
+            target = target.to(device)
+            output = model.forward(data)
+            val_loss = loss(output.squeeze(), target.squeeze())
+            val_losses.append(val_loss.item())
+        avg_val_loss = np.average(val_losses)
+        avg_val_losses.append(avg_val_loss)
+        model.train()
 
-        if epoch > 0 and epoch % 10 == 0:
+        bar.set_description(
+            'train_loss {:.3f}; loss_loss {:.3f}'.format(
+                avg_train_losses[-1], avg_val_losses[-1])
+        )
+
+        if epoch > 0 and epoch % 1 == 0:
             checkpoint_dict = {
                 'epoch': epoch,
                 'model_state_dict': model.state_dict(),
@@ -127,7 +143,8 @@ def train(dataset, num_epochs, batch_size=1):
             }
             timestamp = datetime.now()
             name = 'checkpoint' + str(timestamp) + 'epoch {}.pt'.format(epoch)
-            path = 'checkpoints/' + name
+            path = os.path.join(os.path.abspath(os.path.join(
+                FLAGS.results_save_dir, 'checkpoints')), name)
             torch.save(checkpoint_dict, path)
 
             '''early_stop(avg_val_loss, model)
@@ -138,10 +155,12 @@ def train(dataset, num_epochs, batch_size=1):
 
     return model, avg_val_losses, avg_train_losses
 
+
 def validate(val_set, model):
     correct = 0
     total = 0
-    val_loader = torch.utils.data.DataLoader(val_set, batch_size=1, num_workers=2)
+    val_loader = torch.utils.data.DataLoader(
+        val_set, batch_size=1, num_workers=2)
     #val_loader = torch.nn.utils.rnn.pad_packed_sequence(val_loader)
 
     model.eval()
@@ -156,9 +175,7 @@ def validate(val_set, model):
 
     return accuracy
 
-
-            #batch.set_postfix(loss.item())
-
+    # batch.set_postfix(loss.item())
 
 
 if __name__ == '__main__':
@@ -166,12 +183,16 @@ if __name__ == '__main__':
     flags.DEFINE_integer('n_fft', 512, 'Size of FFT')
     flags.DEFINE_float('preemphasis_coefficient', 0.97,
                        'Coefficient for use in signal preemphasis')
-    flags.DEFINE_integer('num_ceps', 13, ' Number of cepstra in MFCC computation')
+    flags.DEFINE_integer(
+        'num_ceps', 13, ' Number of cepstra in MFCC computation')
     flags.DEFINE_integer('frame_len', 20, 'Frame length in ms')
     flags.DEFINE_integer('frame_shift', 10, 'frame shift in ms')
-    flags.DEFINE_string('csv_file', 'train_data.csv', 'csv file with TIMIT data info')
-    flags.DEFINE_string('root_dir', '../timit', 'root directory for TIMIT data')
-    flags.DEFINE_integer('epochs', 1024, 'number of epochs')
-    flags.DEFINE_float('lr', 0.001, 'learning rate')
+
+    flags.DEFINE_integer('num_epochs', 2, 'Number of epochs')
+    flags.DEFINE_float('lr', 0.001, 'Learning rate')
+    flags.DEFINE_string('dataset_root_dir', 'timit',
+                        'The path to the dataset root directory')
+    flags.DEFINE_string('results_save_dir', 'results',
+                        'The path to the directory where all the results are saved')
 
     app.run(main)
