b9fcd9b26602520beb83aca4d5e1c67915fcace1
alexandertu <alexander.tuoma@gmail.com>
2021-05-16 09:06:35 +0200

    fix

--- optimizers/ema.py
+++ optimizers/ema.py
@@ -10,4 +10,4 @@ class ExponetialMovingAverage(object):
     def step(self):
 
         for param, ema_param in zip(self.model.parameters(), self.ema_model.parameters()):
-            ema_param.data.mul_(self.alpha).add_(param.data, 1 - self.alpha)
+            ema_param.data.mul_(self.alpha).add_(1 - self.alpha)
