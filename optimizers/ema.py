c669ba5f17eac9e5cf25109d4ce2185b0f401dfc
Eirini <ir.stratigi@gmail.com>
2021-05-20 23:41:20 +0200

    Create ema.py
    Add ema optimizer to main branch from mean_teacher_2 branch

---
+++ optimizers/ema.py
@@ -0,0 +1,15 @@
+import torch
+
+
+class ExponetialMovingAverage(object):
+    def __init__(self, model, ema_model, alpha=0.999):
+        self.model = model
+        self.ema_model = ema_model
+        self.alpha = alpha
+
+    def step(self):
+
+        for param, ema_param in zip(self.model.parameters(), self.ema_model.parameters()):
+            ema_param.data.mul_(self.alpha).add_((1 - self.alpha) * param)
+
+            #w = w * 0.99 + 0.01 * new_w
